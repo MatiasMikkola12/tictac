cb67bed215210186080eb6f53aef1f601c06f184
Matias Mikkola <matias.mikkola@blockfi.com>
2022-03-25 11:21:01 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Game from "./pages/Game";
+import Start from "./pages/Start";
+import Finished from "./pages/Finished";
+import useTicTacToe from "./hooks/useTicTacToe"
+export const App = () =>  {
+  const game = useTicTacToe();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      { game.status === "new" && <Start handleStart={game.handleStart} /> }
+      { game.status === "finished" && <Finished name={game.winner} handleRestart={game.handleRestart} /> }
+      { game.status === "started" &&<Game board={game.board} handleClick={game.handleClick} /> }
     </div>
-  )
+  );
 }
-
-export default App
