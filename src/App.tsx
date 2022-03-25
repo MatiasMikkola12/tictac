9e6d77610d6d9f71b67f864a6f8b07a2c3e94a27
Matias Mikkola <matias.mikkola@blockfi.com>
2022-03-25 11:30:59 +0000

    fix up some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import Game from "./pages/Game";
 import Start from "./pages/Start";
 import Finished from "./pages/Finished";
-import useTicTacToe from "./hooks/useTicTacToe"
-export const App = () =>  {
-  const game = useTicTacToe();
+import useTickTackToe from "./hooks/useTicTacToe";
+const App = () => {
+  const game = useTickTackToe();
   return (
     <div className="App">
-      { game.status === "new" && <Start handleStart={game.handleStart} /> }
-      { game.status === "finished" && <Finished name={game.winner} handleRestart={game.handleRestart} /> }
-      { game.status === "started" &&<Game board={game.board} handleClick={game.handleClick} /> }
+      {game.status === "created" && <Start handleStart={game.handleStart} />}
+      {game.status === "finished" && (
+        <Finished name={game.winner} restart={game.handleRestart} />
+      )}
+      {game.status === "started" && (
+        <Game board={game.board} handleClick={game.handleClick} />
+      )}
     </div>
   );
-}
+};
+export default App;
